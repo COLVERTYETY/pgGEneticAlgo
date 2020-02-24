3192f33c9f36a20ca8b35d25a4ba6aae4d2e1b62
COLVERTYETY <niconoob1@gmail.com>
2020-02-25 00:03:50 +0100

    changed for ray opti

--- multi/spatialhash.py
+++ multi/spatialhash.py
@@ -2,9 +2,8 @@ import numpy as np
 
 GRID = []
 
-def ingrid(x,y):
-    global GRID
-    return (x>= 0 and x<len(GRID[0]) and y>=0 and y<len(GRID))
+def ingrid(x,y,ggrid):
+    return (x>= 0 and x<len(ggrid[0]) and y>=0 and y<len(ggrid))
 
 def init(width,height):
     global GRID
@@ -20,16 +19,15 @@ def organise(arrayoflines,cellsize):
     for line in arrayoflines:
         x = int(line[0][0]/cellsize)
         y = int(line[0][1]/cellsize)
-        if ingrid(x,y):
+        if ingrid(x,y,GRID):
             GRID[y][x].append(line)
 
-def getinreach(i,cellsize):
-    global GRID
+def getinreach(i,cellsize,ggrid):
     x = int(i.pos[0]/cellsize)
     y = int(i.pos[1]/cellsize)
     res=[]
     for i in range(-1,2):
         for j in range(-1,2):
-            if ingrid(x+i,y+j):
-                res.extend(GRID[y+j][x+i])
+            if ingrid(x+i,y+j,ggrid):
+                res.extend(ggrid[y+j][x+i])
     return res
